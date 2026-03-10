f42ca0e81880e72ca1229d29b5f11a9fce7eb045
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-03-10 15:05:29 -0300

    feat: create migration and model for Dataset

---
+++ database/migrations/2026_03_10_180323_create_datasets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('datasets', function (Blueprint $table) {
+            $table->id();
+            $table->string('original_filename');
+            $table->string('storage_path');
+            $table->string('parsed_path')->nullable();
+            $table->json('metadata')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('datasets');
+    }
+};
