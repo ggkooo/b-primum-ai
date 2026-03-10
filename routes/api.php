cdbc9feef4c81f94f5aca50810f950eb33c48acf
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-03-10 15:46:26 -0300

    feat: register chat api route

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DatasetController;
+use App\Http\Controllers\ChatController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/datasets/upload', [DatasetController::class, 'upload']);
 Route::post('/datasets/{id}/parse', [DatasetController::class, 'parse']);
+
+Route::post('/chat', [ChatController::class, 'chat']);
 
