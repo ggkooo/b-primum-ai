06f60235fa907f5f590e5a780f8a0faec8e25319
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-03-10 14:57:41 -0300

    feat: register dataset upload route in api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DatasetController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/datasets/upload', [DatasetController::class, 'upload']);
 
